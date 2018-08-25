7a7f8879eee44c3ec451f191012a66d86993c507
ssp <2015suraj.pawar@ves.ac.in>
2018-08-25 21:48:11 +0530

    update

--- assets/js/track.js
+++ assets/js/track.js
@@ -1,7 +1,5 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113925579-3"></script>
-
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'UA-113925579-3');
