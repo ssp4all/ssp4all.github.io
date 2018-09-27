543576cea725e157fee550f704abfe6631641e76
ssp <2015suraj.pawar@ves.ac.in>
2018-09-27 12:24:22 +0530

    Add Rotating Text

---
+++ assets/js/scrolling-text.js
@@ -0,0 +1,18 @@
+// JS script for Rotating text
+var i = 0;
+var txt = 'Avid-Reader. Web-Developer. Deep-Learning-Enthusiast';
+var speed = 400;
+var len = txt.length;
+function typeWriter() {    
+    if (i < len) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    if(i == len)
+    {
+        i=0;
+        document.getElementById("demo").innerHTML = "";
+
+    }
+}
