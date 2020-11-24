cf0d42c1a24390505891284b4ab686efbe92cbdd
Suraj <2015suraj.pawar@ves.ac.in>
2020-11-24 01:53:19 -0500

    minor changes

--- assets/js/scrolling-text.js
+++ assets/js/scrolling-text.js
@@ -1,10 +1,10 @@
 $(function () {
     $(".typed").typed({
-        strings: ["Full-Stack-Web-Developer.", "Software-Enginner.", "Passionate-Reader"],
+        strings: ["Full-Stack-Web-Developer 👨‍💻.", "Software-Enginner 🖥️.", "Data Scientist 📈 ."],
         // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
         stringsElement: null,
         // typing speed
-        typeSpeed: 30,
+        typeSpeed: 0,
         // time before typing starts
         startDelay: 1200,
         // backspacing speed
@@ -14,11 +14,11 @@ $(function () {
         // loop
         loop: true,
         // false = infinite
-        loopCount: 5,
+        loopCount: 100,
         // show cursor
         showCursor: false,
         // character for cursor
-        cursorChar: "|",
+        cursorChar: "🖱️",
         // attribute to type (null == text)
         attr: null,
         // either html or text
